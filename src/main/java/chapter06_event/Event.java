ae369f223721d2721c4b667f809ab36c664a38d5
highright96 <nsw5296@naver.com>
2022-07-21 00:08:22 +0900

    feat: 6장 실습코드 작성

---
+++ src/main/java/chapter06_event/Event.java
@@ -0,0 +1,36 @@
+package chapter06_event;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Event {
+
+    private String subject;
+    private LocalDateTime from;
+    private Duration duration;
+
+    public Event(String subject, LocalDateTime from, Duration duration) {
+        this.subject = subject;
+        this.from = from;
+        this.duration = duration;
+    }
+
+    public void reschedule(RecurringSchedule schedule) {
+        from = LocalDateTime.of(from.toLocalDate().plusDays(daysDistance(schedule)), schedule.getFrom());
+        duration = schedule.getDuration();
+    }
+
+    private long daysDistance(RecurringSchedule schedule) {
+        return schedule.getDayOfWeek().getValue() - from.getDayOfWeek().getValue();
+    }
+
+    public boolean isSatisfied(RecurringSchedule schedule) {
+        if (from.getDayOfWeek() != schedule.getDayOfWeek() ||
+            !from.toLocalTime().equals(schedule.getFrom()) ||
+            !duration.equals(schedule.getDuration())) {
+            //reschedule(schedule);
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/chapter06_event/RecurringSchedule.java
@@ -0,0 +1,36 @@
+package chapter06_event;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class RecurringSchedule {
+
+    private String subject;
+    private DayOfWeek dayOfWeek;
+    private LocalTime from;
+    private Duration duration;
+
+    public RecurringSchedule(String subject, DayOfWeek dayOfWeek, LocalTime from, Duration duration) {
+        this.subject = subject;
+        this.dayOfWeek = dayOfWeek;
+        this.from = from;
+        this.duration = duration;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public LocalTime getFrom() {
+        return from;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+}
